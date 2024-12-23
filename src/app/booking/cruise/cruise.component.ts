aef6f1f0425ab95a90182f8685a3ba42a2386aa2
wayne byard <user00015@gmail.com>
2024-12-23 18:11:02 -0500

    Setting up the cruise page.

--- src/app/booking/cruise/cruise.component.ts
+++ src/app/booking/cruise/cruise.component.ts
@@ -1,8 +1,10 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-cruise',
-  imports: [],
+  imports: [CardModule, NgOptimizedImage],
   templateUrl: './cruise.component.html',
   styleUrl: './cruise.component.scss'
 })
