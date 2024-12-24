d74e754423f38ef14fe24df5f0283ab61e603fbb
wayne byard <user00015@gmail.com>
2024-12-24 15:37:59 -0500

    Cabin booking page done. Also somehow lost the hyperlink css. Whoops

--- src/app/booking/cruise/cruise.component.ts
+++ src/app/booking/cruise/cruise.component.ts
@@ -1,10 +1,11 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-cruise',
-  imports: [CardModule, NgOptimizedImage],
+  imports: [CardModule, NgOptimizedImage, RouterLink, RouterLinkActive],
   templateUrl: './cruise.component.html',
   styleUrl: './cruise.component.scss'
 })
