c23c6598bf42fa20964bee55f4ffb887dbcf3012
wayne byard <user00015@gmail.com>
2025-01-06 20:37:25 -0500

    .

--- src/app/booking/hotel/hotel.component.ts
+++ src/app/booking/hotel/hotel.component.ts
@@ -1,8 +1,11 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-hotel',
-  imports: [],
+  imports: [CardModule, NgOptimizedImage, RouterLink, RouterLinkActive],
   templateUrl: './hotel.component.html',
   styleUrl: './hotel.component.scss'
 })
